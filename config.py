eafd0c10d97953a36426a96e1fbcd045d8b4960f
mnansary <silentassasin0111@gmail.com>
2024-08-21 23:40:56 +0600

    functional app v-0.0.1

---
+++ config.py
@@ -0,0 +1,4 @@
+VECTOR_DB_PATH="data/database" # path to the created database
+MODEL_NAME="llama3.1:70b"
+MODEL_BASE_URL='http://localhost:11434'
+EMBEDDING_MODEL="l3cube-pune/bengali-sentence-bert-nli"
