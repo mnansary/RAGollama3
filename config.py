d2f20af1e5e5b31ebc192364177b20ec1fffe5fb
mnansary <silentassasin0111@gmail.com>
2025-06-03 15:29:52 +0600

    service stable

--- config.py
+++ config.py
@@ -1,15 +1,12 @@
 # path to the created database
-VECTOR_DB_PATH="data/20240901/database"
+VECTOR_DB_PATH="prototype/database"
 
 # The path of the csv file that will be used to create a vectorstore. 
 # # The csv will have only one column and each row entry will be treated as a separate document.  
-DATA_SOURCE="data/source.csv" 
+DATA_SOURCE="prototype/source.csv" 
 
 # The LLM model to be used for RAG
 MODEL_NAME="llama3.3"       
 
-# The model serving url
-MODEL_BASE_URL='http://localhost:11434'  
-
 # The model to use in sentence-transformers for creation embedding
-EMBEDDING_MODEL="l3cube-pune/bengali-sentence-similarity-sbert" 
+EMBEDDING_MODEL="intfloat/multilingual-e5-large"
